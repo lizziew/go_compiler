231f529a7702210bd8b37d3009f2554067d3bdad
Elizabeth Wei <elizabeth.m.wei@gmail.com>
2018-08-19 15:53:58 -0700

    Added first bytecode instruction

---
+++ bytecode/bytecode.go
@@ -0,0 +1,65 @@
+package bytecode
+
+import (
+	"encoding/binary"
+	"fmt"
+)
+
+type Instructions []byte
+type Opcode byte
+
+const (
+	OpConstant Opcode = iota // 1 operand: previous assigned number to constant
+)
+
+// Make instruction from op and operands (Big Endian)
+func Make(op Opcode, operands ...int) []byte {
+	definition, ok := definitions[op]
+	if !ok {
+		return []byte{}
+	}
+
+	numBytes := 1
+	for _, width := range definition.OperandWidths {
+		numBytes += width
+	}
+
+	instruction := make([]byte, numBytes)
+
+	// Add op
+	instruction[0] = byte(op)
+
+	// Add operands
+	offset := 1
+	for i, o := range operands {
+		width := definition.OperandWidths[i]
+
+		switch width {
+		case 2:
+			binary.BigEndian.PutUint16(instruction[offset:], uint16(o))
+		}
+
+		offset += width
+	}
+
+	return instruction
+}
+
+// For debugging
+type Definition struct {
+	Name          string // readability
+	OperandWidths []int  // number of bytes each operand takes up
+}
+
+var definitions = map[Opcode]*Definition{
+	OpConstant: {"OpConstant", []int{2}},
+}
+
+func Lookup(op byte) (*Definition, error) {
+	definition, ok := definitions[Opcode(op)]
+	if !ok {
+		return nil, fmt.Errorf("Opcode %d undefined", op)
+	}
+
+	return definition, nil
+}
---
+++ bytecode/bytecode_test.go
@@ -0,0 +1,30 @@
+package bytecode
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestMake(t *testing.T) {
+	tests := []struct {
+		op       Opcode
+		operands []int
+		expected []byte
+	}{
+		{
+			OpConstant,
+			[]int{65534},
+			[]byte{byte(OpConstant), 255, 254},
+		},
+	}
+
+	for _, test := range tests {
+		instruction := Make(test.op, test.operands...)
+
+		assert.Equal(t, len(instruction), len(test.expected), "Expected length of instruction")
+
+		for i, _ := range test.expected {
+			assert.Equal(t, instruction[i], test.expected[i], "Wrong byte in instruction")
+		}
+	}
+}
