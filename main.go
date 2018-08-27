2492bba24e0037fd514d8f87f0bdc7fe2bf7414a
Elizabeth Wei <elizabeth.m.wei@gmail.com>
2018-08-27 14:05:37 -0700

    Changed main

--- main.go
+++ main.go
@@ -1,74 +1,23 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"go_interpreter/compiler"
-	"go_interpreter/evaluator"
-	"go_interpreter/lexer"
-	"go_interpreter/object"
-	"go_interpreter/parser"
-	"go_interpreter/vm"
-	"time"
+	"go_interpreter/repl"
+	"os"
+	"os/user"
 )
 
-var engine = flag.String("engine", "vm", "use 'vm' or 'eval'")
-
-var input = `
-let fibonacci = fn(x) {
-  if (x == 0) {
-    0
-  } else {
-    if (x == 1) {
-      return 1;
-    } else {
-      fibonacci(x - 1) + fibonacci(x - 2);
-    }
-  }
-};
-fibonacci(35);
-`
-
 func main() {
-	flag.Parse()
-
-	var duration time.Duration
-	var result object.Object
-
-	l := lexer.BuildLexer(input)
-	p := parser.BuildParser(l)
-	program := p.ParseProgram()
-
-	if *engine == "vm" {
-		comp := compiler.BuildCompiler()
-		err := comp.Compile(program)
-		if err != nil {
-			fmt.Printf("compiler error: %s", err)
-			return
-		}
-
-		machine := vm.BuildVM(comp.Bytecode())
-
-		start := time.Now()
-
-		err = machine.Run()
-		if err != nil {
-			fmt.Printf("vm error: %s", err)
-			return
-		}
-
-		duration = time.Since(start)
-		result = machine.LastPoppedStackElem()
-	} else {
-		env := object.NewEnvironment()
-		start := time.Now()
-		result = evaluator.Eval(program, env)
-		duration = time.Since(start)
+	// Get user
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
 	}
 
-	fmt.Printf(
-		"engine=%s, result=%s, duration=%s\n",
-		*engine,
-		result.Inspect(),
-		duration)
+	// Print welcome prompt
+	fmt.Printf("Welcome to the Monkey programming language, %s!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+
+	// Start loop
+	repl.StartLoop(os.Stdin, os.Stdout)
 }
